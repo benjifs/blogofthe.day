ead25785a206cbc455f4252d014a0888de473b66
Benji Encalada Mora <benjifs@users.noreply.github.com>
2024-05-29 02:02:04 -0500

    feat: supportedLanguages

--- src/src.11tydata.js
+++ src/src.11tydata.js
@@ -1,4 +1,5 @@
 module.exports = {
+  "supportedLanguages": [ "en", "es" ],
   "eleventyComputed": {
     "site": data => data[data.lang]?.dates ? data[data.lang]?.dates[data.today] : null,
     "dates": data => data[data.lang]?.dates
