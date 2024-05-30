577aab7dfbba6456834550327d6b8690cbe3a034
Benji Encalada Mora <benjifs@users.noreply.github.com>
2024-05-29 22:23:31 -0500

    feat: add fr and zh

--- src/src.11tydata.js
+++ src/src.11tydata.js
@@ -1,5 +1,9 @@
 module.exports = {
-  "supportedLanguages": [ "en", "es" ],
+  "lang": "en",
+  "locale": "en-US",
+  "dir": "ltr",
+  "title": "Blog of the .Day",
+  "description": "Explore the IndieWeb, the slow web, the independent web, one blog per day",
   "eleventyComputed": {
     "site": data => data[data.lang]?.dates ? data[data.lang]?.dates[data.today] : null,
     "dates": data => data[data.lang]?.dates
